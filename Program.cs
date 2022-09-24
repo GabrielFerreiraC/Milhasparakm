ab2236d28f3d1f3fafd9a857cf85dd6f4437fc39
Gabriel Ferreira <gfdc2204@gmail.com>
2022-09-23 22:11:05 -0300

    Adiciona a solução do exercício

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿double milhas, kms;
+
+Console.WriteLine("Entre com a medida (em milhas):");
+milhas = Convert.ToDouble(Console.ReadLine());
+
+kms = milhas *  1.609;
+
+Console.WriteLine($"{kms:N3} km"); 
